0caedd2237d1f4bdbaf164cb139e8065ec3158f2
Peter Braun <pbraun@redhat.com>
2019-10-01 12:01:24 +0200

    add openshift route dependency

--- pkg/common/auto_detect.go
+++ pkg/common/auto_detect.go
@@ -14,6 +14,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 	"sigs.k8s.io/controller-runtime/pkg/source"
+	_ "github.com/openshift/api/route/v1"
 )
 
 var logAuto = logf.Log.WithName("autodetect")
