ae7e92136bc1e9b161a69b46d58768b8b6ab507d
Sebastian Laskawiec <slaskawi@redhat.com>
2019-11-05 13:08:18 +0100

    Adjust e2e tests timeouts

--- test/e2e/constants.go
+++ test/e2e/constants.go
@@ -6,6 +6,6 @@ const (
 	testKeycloakCRName   = "keycloak-test"
 	cleanupRetryInterval = time.Second * 1
 	cleanupTimeout       = time.Second * 10
-	pollRetryInterval    = time.Second * 15
-	pollTimeout          = time.Second * 480
+	pollRetryInterval    = time.Second * 10
+	pollTimeout          = time.Minute * 10
 )
