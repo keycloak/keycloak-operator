ac439ff19299dca299efa665e1b6a291e23add11
Robert Hoppe <rh@21sheeps.com>
2020-02-12 15:38:26 +0100

    Some fixes to make it building

--- pkg/model/keycloak_ingress.go
+++ pkg/model/keycloak_ingress.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"github.com/integr8ly/grafana-operator/v3/pkg/apis/integreatly/v1alpha1"
 	kc "github.com/keycloak/keycloak-operator/pkg/apis/keycloak/v1alpha1"
 	"k8s.io/api/extensions/v1beta1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -9,43 +10,43 @@ import (
 )
 
 func GetHost(cr *kc.Keycloak) string {
-	if cr.Spec.Ingress == nil {
+	if !cr.Spec.ExternalAccess.Enabled {
 		return ""
 	}
-	return cr.Spec.externalAccess.Hostname
+	return cr.Spec.ExternalAccess.Hostname
 }
 
-func GetPath(cr *cr.Keycloak) string {
-	if cr.Spec.externalAccess == nil {
+func GetPath(cr *kc.Keycloak) string {
+	if !cr.Spec.ExternalAccess.Enabled {
 		return "/"
 	}
-	return cr.Spec.externalAccess.Path
+	return cr.Spec.ExternalAccess.Path
 }
 
-func GetIngressLabels(cr *cr.Keycloak) map[string]string {
-	if cr.Spec.externalAccess == nil {
+func GetIngressLabels(cr *kc.Keycloak) map[string]string {
+	if !cr.Spec.ExternalAccess.Enabled {
 		return nil
 	}
-	return cr.Spec.externalAccess.Labels
+	return cr.Spec.ExternalAccess.Labels
 }
 
 func GetIngressAnnotations(cr *kc.Keycloak, existing map[string]string) map[string]string {
-	if cr.Spec.externalAccess == nil {
+	if !cr.Spec.ExternalAccess.Enabled {
 		return existing
 	}
-	return MergeAnnotations(cr.Spec.externalAccess.Annotations, existing)
+	return MergeAnnotations(cr.Spec.ExternalAccess.Annotations, existing)
 }
 
 func getIngressTLS(cr *kc.Keycloak) []v1beta1.IngressTLS {
-	if cr.Spec.externalAccess == nil {
+	if !cr.Spec.ExternalAccess.Enabled {
 		return nil
 	}
 
-	if cr.Spec.externalAccess.TLSEnabled {
+	if cr.Spec.ExternalAccess.TLSEnabled {
 		return []v1beta1.IngressTLS{
 			{
-				Hosts:      []string{cr.Spec.externalAccess.Hostname},
-				SecretName: cr.Spec.externalAccess.TLSSecretName,
+				Hosts:      []string{cr.Spec.ExternalAccess.Hostname},
+				SecretName: cr.Spec.ExternalAccess.TLSSecretName,
 			},
 		}
 	}
@@ -53,32 +54,32 @@ func getIngressTLS(cr *kc.Keycloak) []v1beta1.IngressTLS {
 }
 
 func getIngressSpec(cr *v1alpha1.Grafana) v1beta1.IngressSpec {
-  return v1beta1.IngressSpec{
-  TLS: getIngressTLS(cr),
-  Rules: []v1beta1.IngressRule{
-    {
-      Host: GetHost(cr),
-      IngressRuleValue: v1beta1.IngressRuleValue{
-        HTTP: &v1beta1.HTTPIngressRuleValue{
-          Paths: []v1beta1.HTTPIngressPath{
-            {
-              Path: GetPath(cr),
-              Backend: v1beta1.IngressBackend{
-                ServiceName: ApplicationName,
-                ServicePort: intstr.FromInt(KeycloakServicePort),
-              },
-            },
-          },
-        },
-      },
-    },
-  },
-}
+	return v1beta1.IngressSpec{
+		TLS: getIngressTLS(cr),
+		Rules: []v1beta1.IngressRule{
+			{
+				Host: GetHost(cr),
+				IngressRuleValue: v1beta1.IngressRuleValue{
+					HTTP: &v1beta1.HTTPIngressRuleValue{
+						Paths: []v1beta1.HTTPIngressPath{
+							{
+								Path: GetPath(cr),
+								Backend: v1beta1.IngressBackend{
+									ServiceName: ApplicationName,
+									ServicePort: intstr.FromInt(KeycloakServicePort),
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
 }
 
 func KeycloakIngress(cr *kc.Keycloak) *v1beta1.Ingress {
 
-  return &v1beta1.Ingress{
+	return &v1beta1.Ingress{
 		ObjectMeta: v1.ObjectMeta{
 			Name:        ApplicationName,
 			Namespace:   cr.Namespace,
@@ -89,9 +90,8 @@ func KeycloakIngress(cr *kc.Keycloak) *v1beta1.Ingress {
 	}
 }
 
-
 func KeycloakIngressReconciled(cr *kc.Keycloak, currentState *v1beta1.Ingress) *v1beta1.Ingress {
-  reconciled := currentState.DeepCopy()
+	reconciled := currentState.DeepCopy()
 	reconciled.Labels = GetIngressLabels(cr)
 	reconciled.Annotations = GetIngressAnnotations(cr, currentState.Annotations)
 	reconciled.Spec = getIngressSpec(cr)
