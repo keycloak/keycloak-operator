29ddb305b47cb969363ed25bc95d5435c53c2f71
Sergio Franco <sfrancog@redhat.com>
2020-03-09 11:05:32 +0000

    fix: Sanitize uppercase letters from resource name
    Modify SanitizeResourceName function to transform uppercase characters into lowercase,
    as kubernetes doesn't allow uppercase characters as part of the resource name:
    https://kubernetes.io/docs/concepts/overview/working-with-objects/names/
    
    Create unit test to cover that function.

--- pkg/model/util_test.go
+++ pkg/model/util_test.go
@@ -66,3 +66,17 @@ func TestUtil_Test_GetServiceEnvVar(t *testing.T) {
 	assert.Equal(t, GetServiceEnvVar("SERVICE_HOST"), "KEYCLOAK_POSTGRESQL_SERVICE_HOST")
 	assert.Equal(t, GetServiceEnvVar("SERVICE_PORT"), "KEYCLOAK_POSTGRESQL_SERVICE_PORT")
 }
+
+func TestUtil_SanitizeResourceName(t *testing.T) {
+	expected := map[string]string{
+		// Allowed characters
+		"test123-_.": "test123--.",
+		// Mixed of allowed characters and disallowed characters
+		"testTEST[(/%^&*,)]123-_.": "testtest123--.",
+	}
+
+	for input, output := range expected {
+		actual := SanitizeResourceName(input)
+		assert.Equal(t, output, actual)
+	}
+}
