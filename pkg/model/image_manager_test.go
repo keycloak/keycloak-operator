c1e551f7bf1cef81257d83e0610bb9f801aabf8c
Sebastian Laskawiec <slaskawi@redhat.com>
2020-05-07 14:08:37 +0200

    KEYCLOAK-13634 Allow a single RHSSO image override

--- pkg/model/image_manager_test.go
+++ pkg/model/image_manager_test.go
@@ -31,3 +31,35 @@ func TestImageManager_test_defining_image_using_environment_variable(t *testing.
 	//then
 	assert.Equal(t, "test", imageChooser.Images[KeycloakImage])
 }
+
+func TestImageManager_test_overriding_rhsso_image_using_environment_variable(t *testing.T) {
+	//given
+	os.Setenv(RHSSOImage, "test")
+
+	//when
+	imageChooser := NewImageManager()
+	os.Unsetenv(RHSSOImage)
+
+	//then
+	assert.Equal(t, "test", imageChooser.Images[RHSSOImage])
+}
+
+func TestImageManager_test_overriding_multiple_images_using_environment_variables(t *testing.T) {
+	//given
+	os.Setenv(RHSSOImage, "RHSSOImage")
+	os.Setenv(RHSSOImageOpenJ9, "RHSSOImageOpenJ9")
+	os.Setenv(RHSSOImageOpenJDK, "RHSSOImageOpenJDK")
+
+	//when
+	imageChooser := NewImageManager()
+	getRHSSOImageResult := imageChooser.getRHSSOImage()
+	os.Unsetenv(RHSSOImage)
+	os.Unsetenv(RHSSOImageOpenJ9)
+	os.Unsetenv(RHSSOImageOpenJDK)
+
+	//then
+	assert.Equal(t, "RHSSOImage", getRHSSOImageResult)
+	assert.Equal(t, "RHSSOImage", imageChooser.Images[RHSSOImage])
+	assert.Equal(t, "RHSSOImageOpenJ9", imageChooser.Images[RHSSOImageOpenJ9])
+	assert.Equal(t, "RHSSOImageOpenJDK", imageChooser.Images[RHSSOImageOpenJDK])
+}
