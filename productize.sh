aaf0377d882ff4f2efb2aac026f0b600581715a3
Hynek Mlnarik <hmlnarik@redhat.com>
2020-05-13 13:37:52 +0200

    KEYCLOAK-13616 Update productization script

--- productize.sh
+++ productize.sh
@@ -2,7 +2,7 @@
 
 LABELS=$(cat <<-END
 LABEL \
-com.redhat.component="redhat-sso-7-sso74-operator-rhel8-container"  \
+com.redhat.component="redhat-sso-7-sso74-rhel8-operator-container"  \
 description="Red Hat Single Sign-On 7.4 Operator container image, based on the Red Hat Universal Base Image 8 Minimal container image" \
 summary="Red Hat Single Sign-On 7.4 Operator container image, based on the Red Hat Universal Base Image 8 Minimal container image" \
 version="7.4" \
@@ -14,7 +14,16 @@ maintainer="Red Hat Single Sign-On Team"
 END
 )
 
-sed -i 's/registry.access.redhat.com\/ubi8\/ubi-minimal:8.1/ubi8-minimal:8-released/' Dockerfile
-sed -i -e 's/FROM registry.access.redhat.com\/ubi8\/ubi-minimal:[0-9.]*/FROM ubi8-minimal:8-released/' \
+GIT_REPOSITORY="${1:-https://github.com/keycloak/keycloak-operator.git}"
+GIT_BRANCH="${2:-master}"
+
+GIT_LAST_COMMIT_HASH=$(git ls-remote "${GIT_REPOSITORY}" "${GIT_BRANCH}" | awk '{ print $1}')
+
+# Ensure that commit ID is there to prevent returning cached build upon a new commit
+GIT_COMMAND="git clone --single-branch --branch ${GIT_BRANCH} ${GIT_REPOSITORY} .  #${GIT_LAST_COMMIT_HASH}"
+
+sed -i \
+    -e 's/FROM registry.access.redhat.com\/ubi8\/ubi-minimal:[0-9.]*/FROM ubi8-minimal:8-released/' \
     -e "s/##LABELS/$LABELS/g" \
+    -e "s,^## *RUN git clone .*\$,RUN $GIT_COMMAND," \
     Dockerfile
