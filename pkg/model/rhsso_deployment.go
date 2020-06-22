93b859e514960c80f66109762df8b62d5a6f7a73
akoserwa <akoserwa@redhat.com>
2020-06-22 22:33:07 +0530

    KEYCLOAK-14559 updating ca bundle env variable with wildcard ca value

--- pkg/model/rhsso_deployment.go
+++ pkg/model/rhsso_deployment.go
@@ -88,7 +88,7 @@ func getRHSSOEnv(cr *v1alpha1.Keycloak, dbSecret *v1.Secret) []v1.EnvVar {
 		},
 		{
 			Name:  "X509_CA_BUNDLE",
-			Value: "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt",
+			Value: "/var/run/secrets/kubernetes.io/serviceaccount/*.crt",
 		},
 	}
 
