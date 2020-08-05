6d95a7a1192d9dd82fdfb8441a3660a976ea7d06
Andrew Papia <apapia@cogitocorp.com>
2020-08-04 22:19:49 -0400

    try some more things

--- keycloak-debug/change-database.sh
+++ keycloak-debug/change-database.sh
@@ -7,9 +7,11 @@ export ROOT_LOGLEVEL="TRACE"
 
 cd /opt/jboss/keycloak
 
+echo "Setting loglevel"
 bin/jboss-cli.sh --file=/opt/jboss/tools/cli/loglevel.cli
+echo "Starting standalone config"
 bin/jboss-cli.sh --file=/opt/jboss/tools/cli/databases/$DB_VENDOR/standalone-configuration.cli
 rm -rf /opt/jboss/keycloak/standalone/configuration/standalone_xml_history
-
+echo "Starting HA config"
 bin/jboss-cli.sh --file=/opt/jboss/tools/cli/databases/$DB_VENDOR/standalone-ha-configuration.cli
 rm -rf standalone/configuration/standalone_xml_history/current/*
