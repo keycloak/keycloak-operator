e5db369241dc3aa3731591f93782ba25e5739c1d
Andrew Papia <apapia@cogitocorp.com>
2020-08-04 23:10:45 -0400

    add connect string

--- keycloak-debug/change-database.sh
+++ keycloak-debug/change-database.sh
@@ -8,10 +8,10 @@ export ROOT_LOGLEVEL="TRACE"
 cd /opt/jboss/keycloak
 
 echo "Setting loglevel"
-bin/jboss-cli.sh --file=/opt/jboss/tools/cli/loglevel.cli
+bin/jboss-cli.sh --connect --file=/opt/jboss/tools/cli/loglevel.cli
 echo "Starting standalone config"
-bin/jboss-cli.sh --file=/opt/jboss/tools/cli/databases/$DB_VENDOR/standalone-configuration.cli
+bin/jboss-cli.sh --connect --file=/opt/jboss/tools/cli/databases/$DB_VENDOR/standalone-configuration.cli
 rm -rf /opt/jboss/keycloak/standalone/configuration/standalone_xml_history
 echo "Starting HA config"
-bin/jboss-cli.sh --file=/opt/jboss/tools/cli/databases/$DB_VENDOR/standalone-ha-configuration.cli
+bin/jboss-cli.sh --connect --file=/opt/jboss/tools/cli/databases/$DB_VENDOR/standalone-ha-configuration.cli
 rm -rf standalone/configuration/standalone_xml_history/current/*
