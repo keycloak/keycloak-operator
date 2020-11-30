efcd410bb5ad004d3364ef3d7f62c470c55a5722
Trevor Loranger <trevor.loranger@keysight.com>
2020-11-30 01:06:36 -0500

    KEYCLOAK-16488 Add AuthZ settings to client CRD

--- test/e2e/keycloak_client_test.go
+++ test/e2e/keycloak_client_test.go
@@ -11,6 +11,11 @@ import (
 const (
 	clientName         = "test-client"
 	externalClientName = "test-client-external"
+	roleName           = "test-client-role"
+	scopeName          = "test-client-scope"
+	resourceName       = "test-client-resource"
+	policyName         = "test-client-policy"
+	permissionName     = "test-client-permission"
 )
 
 func NewKeycloakClientsCRDTestStruct() *CRDTestStruct {
@@ -59,29 +64,77 @@ func getKeycloakClientCR(namespace string, external bool) *keycloakv1alpha1.Keyc
 				MatchLabels: labels,
 			},
 			Client: &keycloakv1alpha1.KeycloakAPIClient{
-				ID:                        id,
-				ClientID:                  id,
-				Name:                      id,
-				SurrogateAuthRequired:     false,
-				Enabled:                   true,
-				BaseURL:                   "https://operator-test.url/client-base-url",
-				AdminURL:                  "https://operator-test.url/client-admin-url",
-				RootURL:                   "https://operator-test.url/client-root-url",
-				Description:               "Client used within operator tests",
-				WebOrigins:                []string{"https://operator-test.url"},
-				BearerOnly:                false,
-				ConsentRequired:           false,
-				StandardFlowEnabled:       true,
-				ImplicitFlowEnabled:       false,
-				DirectAccessGrantsEnabled: true,
-				ServiceAccountsEnabled:    false,
-				PublicClient:              true,
-				FrontchannelLogout:        false,
-				Protocol:                  "openid-connect",
-				FullScopeAllowed:          true,
-				NodeReRegistrationTimeout: -1,
-				DefaultClientScopes:       []string{"profile"},
-				OptionalClientScopes:      []string{"microprofile-jwt"},
+				ID:                           id,
+				ClientID:                     id,
+				Secret:                       id,
+				Name:                         id,
+				SurrogateAuthRequired:        false,
+				Enabled:                      true,
+				BaseURL:                      "https://operator-test.url/client-base-url",
+				AdminURL:                     "https://operator-test.url/client-admin-url",
+				RootURL:                      "https://operator-test.url/client-root-url",
+				Description:                  "Client used within operator tests",
+				WebOrigins:                   []string{"https://operator-test.url"},
+				BearerOnly:                   false,
+				ConsentRequired:              false,
+				StandardFlowEnabled:          true,
+				ImplicitFlowEnabled:          false,
+				DirectAccessGrantsEnabled:    true,
+				ServiceAccountsEnabled:       true,
+				PublicClient:                 false,
+				FrontchannelLogout:           false,
+				Protocol:                     "openid-connect",
+				FullScopeAllowed:             true,
+				NodeReRegistrationTimeout:    -1,
+				DefaultClientScopes:          []string{"profile"},
+				OptionalClientScopes:         []string{"microprofile-jwt"},
+				DefaultRoles:                 []string{roleName},
+				ClientAuthenticatorType:      "client-secret",
+				AuthorizationServicesEnabled: true,
+				AuthorizationSettings: &keycloakv1alpha1.KeycloakResourceServer{
+					AllowRemoteResourceManagement: false,
+					DecisionStrategy:              "AFFIRMATIVE",
+					Policies: []keycloakv1alpha1.KeycloakPolicy{{
+						ID:               policyName,
+						Name:             policyName,
+						Description:      policyName,
+						Type:             "role",
+						Logic:            "POSITIVE",
+						DecisionStrategy: "UNANIMOUS",
+						Config: map[string]string{
+							"roles": "[{\"id\":\"" + id + "/" + roleName + "\",\"required\":true}]",
+						},
+					},
+						{
+							ID:               permissionName,
+							Name:             permissionName,
+							Description:      permissionName,
+							Type:             "scope",
+							Logic:            "POSITIVE",
+							DecisionStrategy: "UNANIMOUS",
+							Config: map[string]string{
+								"resources":     "[\"" + resourceName + "\"]",
+								"scopes":        "[\"" + scopeName + "\"]",
+								"applyPolicies": "[\"" + policyName + "\"]",
+							},
+						}},
+					PolicyEnforcementMode: "ENFORCING",
+					Resources: []keycloakv1alpha1.KeycloakResource{{
+						ID:                 resourceName,
+						DisplayName:        resourceName,
+						Name:               resourceName,
+						OwnerManagedAccess: false,
+						Scopes: []keycloakv1alpha1.KeycloakScope{{
+							Name: scopeName,
+						}},
+						Uris: []string{resourceName + "/*"},
+					}},
+					Scopes: []keycloakv1alpha1.KeycloakScope{{
+						ID:          scopeName,
+						Name:        scopeName,
+						DisplayName: scopeName,
+					}},
+				},
 			},
 		},
 	}
