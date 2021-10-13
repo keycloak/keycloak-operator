04d98673a0dfef6d8ecfe2af0c791087a4c317a7
Jonathan Vila <jvilalop@redhat.com>
2021-10-13 11:07:37 +0200

    KEYCLOAK-18388 [PR] added explanatory info and cleaned condition on postgresql service getSpec
    Signed-off-by: Jonathan Vila <jvilalop@redhat.com>

--- pkg/model/postgresql_service.go
+++ pkg/model/postgresql_service.go
@@ -14,19 +14,21 @@ func getSpec(dbSecret *v1.Secret, serviceTypeExternal bool) v1.ServiceSpec {
 	spec := v1.ServiceSpec{}
 	isIPAddress := dbSecret != nil && dbSecret.Data[DatabaseSecretExternalAddressProperty] != nil && IsIP(dbSecret.Data[DatabaseSecretExternalAddressProperty])
 
-	if serviceTypeExternal && !isIPAddress {
-		spec.Type = v1.ServiceTypeExternalName
+	if serviceTypeExternal {
+		// KEYCLOAK-18388 no sense for selectors when we have an external DB
 		spec.Selector = nil
-		spec.ExternalName = GetExternalDatabaseHost(dbSecret)
+		// ExternalName doesn't work with IP addresses
+		if !isIPAddress {
+			spec.Type = v1.ServiceTypeExternalName
+			spec.ExternalName = GetExternalDatabaseHost(dbSecret)
+		} else {
+			spec.Type = v1.ServiceTypeClusterIP
+		}
 	} else {
 		spec.Type = v1.ServiceTypeClusterIP
-		if !isIPAddress {
-			spec.Selector = map[string]string{
-				"app":       ApplicationName,
-				"component": PostgresqlDeploymentComponent,
-			}
-		} else {
-			spec.Selector = nil
+		spec.Selector = map[string]string{
+			"app":       ApplicationName,
+			"component": PostgresqlDeploymentComponent,
 		}
 	}
 
