96df64e8f110ecd31518267d71d522c42ebb59ac
Jonathan Vila <jvilalop@redhat.com>
2021-11-11 21:01:47 +0100

    #404 - setting podlabels in reconcile KEYCLOAK-99999
    Signed-off-by: Jonathan Vila <jvilalop@redhat.com>

--- pkg/model/keycloak_deployment.go
+++ pkg/model/keycloak_deployment.go
@@ -253,6 +253,7 @@ func KeycloakDeploymentSelector(cr *v1alpha1.Keycloak) client.ObjectKey {
 
 func KeycloakDeploymentReconciled(cr *v1alpha1.Keycloak, currentState *v13.StatefulSet, dbSecret *v1.Secret) *v13.StatefulSet {
 	reconciled := currentState.DeepCopy()
+	reconciled.ObjectMeta.Labels = AddPodLabels(cr, reconciled.ObjectMeta.Labels)
 	reconciled.ResourceVersion = currentState.ResourceVersion
 	reconciled.Spec.Replicas = SanitizeNumberOfReplicas(cr.Spec.Instances, false)
 	reconciled.Spec.Template.Spec.Volumes = KeycloakVolumes(cr)
