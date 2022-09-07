5a0a514f8d2ab2ae1ef4d2afe2639ba2e4924fbc
Joshua Smith <jossmith.ctr@kr.af.mil>
2022-09-07 12:34:32 -0400

    Add missing flows to keycloakRealmWithAuthenticatorFlowTest

--- test/e2e/keycloak_realm_test.go
+++ test/e2e/keycloak_realm_test.go
@@ -333,6 +333,33 @@ func getDirectGrantFlow() keycloakv1alpha1.KeycloakAPIAuthenticationFlow {
 	}
 }
 
+func getResetCredentialsFlow() keycloakv1alpha1.KeycloakAPIAuthenticationFlow {
+	return keycloakv1alpha1.KeycloakAPIAuthenticationFlow{
+		Alias:                    "reset credentials",
+		ProviderID:               "basic-flow",
+		TopLevel:                 true,
+		AuthenticationExecutions: []keycloakv1alpha1.KeycloakAPIAuthenticationExecution{},
+	}
+}
+
+func getClientAuthenticationFlow() keycloakv1alpha1.KeycloakAPIAuthenticationFlow {
+	return keycloakv1alpha1.KeycloakAPIAuthenticationFlow{
+		Alias:                    "clients",
+		ProviderID:               "client-flow",
+		TopLevel:                 true,
+		AuthenticationExecutions: []keycloakv1alpha1.KeycloakAPIAuthenticationExecution{},
+	}
+}
+
+func getDockerAuthenticationFlow() keycloakv1alpha1.KeycloakAPIAuthenticationFlow {
+	return keycloakv1alpha1.KeycloakAPIAuthenticationFlow{
+		Alias:                    "docker auth",
+		ProviderID:               "basic-flow",
+		TopLevel:                 true,
+		AuthenticationExecutions: []keycloakv1alpha1.KeycloakAPIAuthenticationExecution{},
+	}
+}
+
 func keycloakRealmWithAuthenticatorFlowTest(t *testing.T, framework *test.Framework, ctx *test.Context, namespace string) error {
 	keycloakRealmCR := getKeycloakRealmCR(namespace)
 
@@ -388,7 +415,7 @@ func keycloakRealmWithAuthenticatorFlowTest(t *testing.T, framework *test.Framew
 		},
 	}
 
-	keycloakRealmCR.Spec.Realm.AuthenticationFlows = []keycloakv1alpha1.KeycloakAPIAuthenticationFlow{autoLinkFlow, getBrowserFlow(), getRegistrationFlow(), getDirectGrantFlow()}
+	keycloakRealmCR.Spec.Realm.AuthenticationFlows = []keycloakv1alpha1.KeycloakAPIAuthenticationFlow{autoLinkFlow, getBrowserFlow(), getRegistrationFlow(), getDirectGrantFlow(), getResetCredentialsFlow(), getClientAuthenticationFlow(), getDockerAuthenticationFlow()}
 
 	keycloakRealmCR.Spec.Realm.IdentityProviders = []*keycloakv1alpha1.KeycloakIdentityProvider{identityProvider}
 
